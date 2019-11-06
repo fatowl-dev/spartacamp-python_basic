c97f91af83c928021f9db9899f8b03e0b9199c20
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-06 12:36:23 +0900

    課題B-6 短く書き直した

--- task_b-6.py
+++ task_b-6.py
@@ -1,10 +1,16 @@
 import random
 sur_num = int(input('サイコロの面の数は? : '))
 trial = int(input('何回振りますか? : '))
-print('[', end='')
+# print('[', end='')
+# for i in range(trial):
+#     print(random.randrange(1, sur_num + 1), end='')
+#     if i == trial - 1:
+#         print(']')
+#     else:
+#         print(', ', end='')
+
+# 短いバージョン
+num_list = []
 for i in range(trial):
-    print(random.randrange(1, sur_num + 1), end='')
-    if i == trial - 1:
-        print(']')
-    else:
-        print(', ', end='')
+    num_list.append(random.randint(1, sur_num))
+print(num_list)
