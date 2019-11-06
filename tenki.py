580904a977c162729676108d77c9ac0db5e615bc
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-06 12:24:42 +0900

    課題B-5 イテレーションを使って書き直した

--- tenki.py
+++ tenki.py
@@ -14,26 +14,35 @@ def main():
     ]
 
     # Q1. 全国の平均気温を計算してください(9.5となればOK)
-    sum_temp = 0
-    for info in weather_information:
-        sum_temp += info['temperature']
-    average_temp = sum_temp / len(weather_information)
+    # sum_temp = 0
+    # for info in weather_information:
+    #     sum_temp += info['temperature']
+    # average_temp = sum_temp / len(weather_information)
+    # print(average_temp)
+
+    # イテレータを使った書き方に変更
+    average_temp = sum(d['temperature'] for d in weather_information) / len(weather_information)
     print(average_temp)
 
     # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
-    station_list = []
-    for info in weather_information:
-        if info['prefecture'] == '大阪府':
-            station_list.append(info['station'])
+    # station_list = []
+    # for info in weather_information:
+    #     if info['prefecture'] == '大阪府':
+    #         station_list.append(info['station'])
+    # print(','.join(station_list))
+    # イテレータを使った書き方に変更
+    station_list = [d['station'] for d in weather_information if d['prefecture'] == '大阪府']
     print(','.join(station_list))
 
     # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
-    temps = []
-    for info in weather_information:
-        if info['prefecture'] == '福岡県':
-            temps.append(info['temperature'])
-    print(sum(temps) / len(temps))
-
+    # temps = []
+    # for info in weather_information:
+    #     if info['prefecture'] == '福岡県':
+    #         temps.append(info['temperature'])
+    # print(sum(temps) / len(temps))
+    # イテレータを使った書き方に変更
+    hukuoka_temps = [d['temperature'] for d in weather_information if d['prefecture'] == '福岡県']
+    print(sum(hukuoka_temps) / len(hukuoka_temps))
 
 
 if __name__ == '__main__':
